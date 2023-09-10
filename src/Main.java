9c7ccf946891f6cdbe48279a02b3eaaa427437e4
inga911 <inga.banaityte221@gmail.com>
2023-09-10 20:50:22 +0200

    made printBonusDatesBetween function

--- src/Main.java
+++ src/Main.java
@@ -1,88 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        int fromYear = 2010;
-        int toYear = 2015;
+        int fromYear = 2015;
+        int toYear = 2010;
 
         if (fromYear < toYear) {
             for (int i = fromYear; i <= toYear; i++) {
-                int month = i / 100;
-                int day = i % 100;
-
-                //          year to string
-                int toString = i;
-                String s = String.valueOf(toString);
-
-                //            splint into 2 parts
-                final int middle = s.length() / 2;
-                String[] parts = {s.substring(0, middle), s.substring(middle)};
-
-                //            reverse day
-                String strReverseDay = parts[0];
-                byte[] strAsByteArrayDay = strReverseDay.getBytes();
-                byte[] resultDay = new byte[strAsByteArrayDay.length];
-                for (int a = 0; a < strAsByteArrayDay.length; a++) {
-                    resultDay[a] = strAsByteArrayDay[strAsByteArrayDay.length - a - 1];
-                }
-
-                //            reverse month
-                String strReverseMonth = parts[1];
-                byte[] strAsByteArrayMonth = strReverseMonth.getBytes();
-                byte[] resultMonth = new byte[strAsByteArrayMonth.length];
-                for (int a = 0; a < strAsByteArrayMonth.length; a++) {
-                    resultMonth[a] = strAsByteArrayMonth[strAsByteArrayMonth.length - a - 1];
-                }
-                //                month is <=12 && day is <= 31
-                int m = Integer.parseInt(new String(resultMonth));
-                int d = Integer.parseInt(new String(resultDay));
-                if (1 <= m && m <= 12 && 1 <= d && d <= 31) {
-                    System.out.println("-----");
-                    System.out.println(i + " year");
-                    System.out.println("New date: " + i + '-' + new String(resultMonth) + '-' + new String(resultDay));
-
-                }
+                printBonusDatesBetween(i);
             }
-
-        }else {
-            for (int iDesc = fromYear; iDesc >= toYear; iDesc--) { // Descending order
-                int monthDesc = iDesc / 100;
-                int dayDesc = iDesc % 100;
-
-                //          year to string
-                int toStringDesc = iDesc;
-                String s = String.valueOf(toStringDesc);
-
-                //            splint into 2 parts
-                final int middleDesc = s.length() / 2;
-                String[] partsDesc = {s.substring(0, middleDesc), s.substring(middleDesc)};
-
-                //            reverse day
-                String strReverseDayDesc = partsDesc[0];
-                byte[] strAsByteArrayDayDesc = strReverseDayDesc.getBytes();
-                byte[] resultDayDesc = new byte[strAsByteArrayDayDesc.length];
-                for (int a = 0; a < strAsByteArrayDayDesc.length; a++) {
-                    resultDayDesc[a] = strAsByteArrayDayDesc[strAsByteArrayDayDesc.length - a - 1];
-                }
-
-                //            reverse month
-                String strReverseMonthDesc = partsDesc[1];
-                byte[] strAsByteArrayMonthDesc = strReverseMonthDesc.getBytes();
-                byte[] resultMonthDesc = new byte[strAsByteArrayMonthDesc.length];
-                for (int a = 0; a < strAsByteArrayMonthDesc.length; a++) {
-                    resultMonthDesc[a] = strAsByteArrayMonthDesc[strAsByteArrayMonthDesc.length - a - 1];
-                }
-
-                //            month is <=12 && day is <= 31
-                int mDesc = Integer.parseInt(new String(resultMonthDesc));
-                int dDesc = Integer.parseInt(new String(resultDayDesc));
-                if (1 <= mDesc && mDesc <= 12 && 1 <= dDesc && dDesc <= 31) {
-                    System.out.println("-----");
-                    System.out.println(iDesc + " year");
-                    System.out.println("New date: " + iDesc + '-' + new String(resultMonthDesc) + '-' + new String(resultDayDesc));
-
-                }
+        } else {
+            for (int i = fromYear; i >= toYear; i--) {
+                printBonusDatesBetween(i);
             }
+        }
+    }
 
+    private static void printBonusDatesBetween(int date) {
+        int month = date / 100;
+        int year = date % 100;
 
+        //          year to string
+        int toStringDesc = date;
+        String s = String.valueOf(toStringDesc);
+
+        //            split into 2 parts
+        final int middleDesc = s.length() / 2;
+        String[] partsDesc = {s.substring(0, middleDesc), s.substring(middleDesc)};
+
+        //            reverse day
+        String reverseDay = partsDesc[0];
+        byte[] strAsByteDay = reverseDay.getBytes();
+        byte[] resultDayDesc = new byte[strAsByteDay.length];
+        for (int a = 0; a < strAsByteDay.length; a++) {
+            resultDayDesc[a] = strAsByteDay[strAsByteDay.length - a - 1];
+        }
+
+        //            reverse month
+        String reverseMonth = partsDesc[1];
+        byte[] strAsByteMonth = reverseMonth.getBytes();
+        byte[] resultMonthDesc = new byte[strAsByteMonth.length];
+        for (int a = 0; a < strAsByteMonth.length; a++) {
+            resultMonthDesc[a] = strAsByteMonth[strAsByteMonth.length - a - 1];
+        }
+
+        //            month is <=12 && day is <= 31
+        int reversedMonth = Integer.parseInt(new String(resultMonthDesc));
+        int reversedDay = Integer.parseInt(new String(resultDayDesc));
+        if (1 <= reversedMonth && reversedMonth <= 12 && 1 <= reversedDay && reversedDay <= 31) {
+            System.out.println("-----");
+            System.out.println(date + " year");
+            System.out.println("New date: " + date + '-' + new String(resultMonthDesc) + '-' + new String(resultDayDesc));
 
         }
     }
